692f1dc39c8e0844ccf625bd11dc9888fd36b514
Hugo HOW-CHOONG <hhow-cho@e3r8p13.42.fr>
2019-04-29 11:25:12 +0200

    install test for d o u x

--- tests/tests/includes/main.h
+++ tests/tests/includes/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 # define MAIN_H
 
-int	conv_f_launcher(void);
+int	conv_i_launcher(void);
+int	conv_u_launcher(void);
+int	conv_o_launcher(void);
+int	conv_x_launcher(void);
 /*PROTOTYPES_HERE*/
 
 typedef struct		s_launcher
@@ -12,7 +15,10 @@ typedef struct		s_launcher
 
 t_launcher			g_launchtab[] =
 {
-	{"conv_f", conv_f_launcher},
+	{"conv_i", conv_i_launcher},
+	{"conv_u", conv_u_launcher},
+	{"conv_o", conv_o_launcher},
+	{"conv_x", conv_x_launcher},
 	/*FCTS_HERE*/
 	{"", NULL}
 };
