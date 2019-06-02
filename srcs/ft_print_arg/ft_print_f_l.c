b76da5d8f99534090aaffc52180d12abee7ef1f3
Hugo How <hugohowchoong@gmail.com>
2019-06-02 19:47:13 +0200

    change %f -> %lf

--- srcs/ft_print_arg/ft_print_f_l.c
+++ srcs/ft_print_arg/ft_print_f_l.c
@@ -6,53 +6,56 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 23:56:31 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/06/02 12:11:04 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/06/02 19:46:29 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
 
-static char				*get_bin_floating_point(long double nb)
+static char				*get_bin_floating_point(double nb)
 {
 	char			*output;
 	int				i;
-	unsigned char	t[10 + 1];
+	unsigned char	t[sizeof(double) + 1];
 	char tmp[10];
-	if (!(output = (char *)ft_memalloc(sizeof(*output) * 100)))
+	if (!(output = (char *)ft_memalloc(sizeof(char) * 100)))
 		return (NULL);
-	ft_memcpy(t, &nb, 10);
-	i = (10) - 1;
+	ft_memcpy(t, &nb, sizeof(double));
+	i = (sizeof(double)) - 1;
 	while (i != -1)
 	{
 		output = ft_strcat(output, ft_get_binary(t[i], tmp));
 		i--;
 	}
-
 	return (output);
 }
-
-
-char			*ft_print_f_l(va_list *ap, t_flag *flag)
+char				*ft_print_f_l(va_list *ap, t_flag *flag)
 {
 	char			*output;
-	long double			tmp;
+	double			tmp;
 	size_t			size_allocation;
 	int				sign;
 	char 			*to_free;
-	
-	size_allocation = 6000;
-	tmp = (long double)va_arg(*ap, double);
+	size_allocation = 4096;
+	tmp = (double)va_arg(*ap, double);
 	to_free = get_bin_floating_point(tmp);
 	sign = to_free[0] == '1' ? -1 : 1;
-	output = ft_itoa_f_l(tmp, to_free, flag, size_allocation);
-	if (flag->precision == -1)
-		output = ft_bigint_round(output, 6, size_allocation);
-	else
-		output = ft_bigint_round(output, flag->precision, size_allocation);
+	output = ft_itoa_f(tmp, to_free, flag, size_allocation);
+	if  (ft_strchr(output, 'i') == 0 && ft_strchr(output, 'n') == 0 )
+	{
+		if (flag->precision == -1)
+			output = ft_bigint_round(output, 6, size_allocation);
+		else
+			output = ft_bigint_round(output, flag->precision, size_allocation);
+		if (output[0] == '.')
+			output = ft_str_join("0", output, flag);
+		if (ft_strlen(output) == 0)
+			output = ft_strcpy(output, "0");
+		if (flag->hash && flag->precision == 0)
+			output = ft_str_join_r(output, ".", flag);
+	}
 	output = ft_apply_padding_nb(output, flag, sign);
-	if (flag->hash && flag->precision == 0)
-		output = ft_str_join_r(output, ".", flag);
 	ft_memdel((void **)&to_free);
 	return (output);
 }
